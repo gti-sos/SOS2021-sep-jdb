d12d926f935b8f610edf1f33e2e7a5a9408ebef9
jdblanco <diezblancojuan@gmail.com>
2021-08-17 17:36:05 +0200

    primeros pasos

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hola mundo");
+var express= require("express");
+
+var app=express();
+var path=require("path");
+var port=10000;
+
+
+//ruta estatica(ejecuta todo lo de dentro de public de la forma:/archivo.extension)
+app.use("/",express.static(path.join(__dirname + "/public")));
+
+
+//ruta dinamica
+app.get("/home",(req,res)=>{
+    res.send("<html><body><h1>HOLA</h1></body></html>");
+
+});
+
+//callback
+app.listen(port,()=>{
+    //al colocarse de esta forma se  ejecuta el mensaje cuando el servidor este listo
+    console.log(`server ready to listen on port ${port}`);
+});
+
